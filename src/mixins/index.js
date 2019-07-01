8d10fa3d835668475c5824b9e838cd854b4924e9
xilanhuadechuntian <Lensgcx@163.com>
2019-07-01 16:21:33 +0800

    1

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -1,10 +1,10 @@
-import funcId from './funcId';
+import funcId from './Common/FuncId';
 import other from './other';
-import roleButton from './role-button/index';
+import roleButton from './system/Role-button/index';
 import searchConditions from './search-conditions';
 
 
-import table from './table';
+import table from './system/Table';
 
 export default {
   mixins: [funcId, other, roleButton, searchConditions, table],
--- src/mixins/other.js
+++ src/mixins/other.js
@@ -1,9 +1,7 @@
-
 export default {
   data() {
     return {
-      Mix_thlist: { thlistDefault: [] }, //筛选器中所需的表头数据
-      Mix_loadState: { data: false, head: false }, //表格数据加载状态
+      Mix_loadState: {data: false, head: false}, //表格数据加载状态
       Mix_treeNodeObj: {}
     };
   },
@@ -18,7 +16,5 @@ export default {
   created() {
 
   },
-  methods: {
-
-  },
+  methods: {},
 };
