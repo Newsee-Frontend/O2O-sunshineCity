52a5a52b666d636811a21949a0e00db03bebfb25
zhouxia <zhouxia@servyou.com.cn>
2019-07-02 16:08:16 +0800

    增加select tree
    增加select tree

---
+++ src/components/Biz/Biz-select-tree/Biz-organize-select-tree/Biz-organize-select-tree.vue
@@ -0,0 +1,38 @@
+<template>
+  <ns-select-tree
+    :size="size"
+    appendToBody
+    v-model="selectModel"
+    :options="options"
+    @input="emitInput"
+  ></ns-select-tree>
+</template>
+<script>
+  export default {
+    name: 'biz-organize-select-tree',
+
+    props: {
+      size: String
+    },
+
+    data(){
+      return{
+        selectModel: this.value,
+        options: null
+      }
+    },
+
+    watch: {
+      value(val){
+        this.selectModel = val;
+      }
+    },
+
+    methods: {
+      emitInput: function(){
+        this.$emit('input', this.selectModel);
+      }
+    }
+  }
+
+</script>
--- src/components/Biz/index.js
+++ src/components/Biz/index.js
@@ -7,6 +7,9 @@ import nsGrids from './Biz-grid/ns-grids';
 //tree
 import BizOrganizeTree from './Biz-tree/Biz-organize-tree/Biz-organize-tree';
 
+//select-tree
+import BizOrgnizeSelectTree from './Biz-select-tree/Biz-organize-select-tree/Biz-organize-select-tree.vue'
+
 
 //conditions
 import SearchConditions from './Biz-search-conditions/Biz-search-conditions';
@@ -20,6 +23,9 @@ Vue.component('ns-grids', nsGrids);
 Vue.component('biz-organize-tree', BizOrganizeTree);
 
 Vue.component('biz-search-conditions', SearchConditions);
+
+//select tree
+Vue.component('biz-organize-select-tree', BizOrgnizeSelectTree)
 
 
 
