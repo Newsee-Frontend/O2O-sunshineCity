959bee4587708ead36a7ae6515d2127291d1739a
xilanhuadechuntian <Lensgcx@163.com>
2019-07-02 09:25:38 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import Core from './modules/common/Core';
+import Core from './modules/Common/Core';
 import User from './modules/User/User';
-import Sidebar from './modules/common/Sidebar';
-import PageInfo from './modules/common/PageInfo';
-import PageTabs from './modules/common/PageTabs';
+import Sidebar from './modules/Common/Sidebar';
+import PageInfo from './modules/Common/PageInfo';
+import PageTabs from './modules/Common/PageTabs';
 
 import Form from './modules/Form';
 
 import OrganizeTree from './modules/Tree/Organize-tree';
 import HouseTree from './modules/Tree/House-tree';
 
-import SearchConditions from './modules/common/SearchConditions';
-import RoleButton from './modules/common/RoleButton';
+import SearchConditions from './modules/Common/SearchConditions';
+import RoleButton from './modules/Common/RoleButton';
 import TreeData from './modules/TreeData';
 import Table from './modules/System/Table/Table';
 import RequestHead from './modules/RequestHeader'
