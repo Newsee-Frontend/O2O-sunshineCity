161634e6ecf23022017d55c8d348b8d67866e497
xilanhuadechuntian <Lensgcx@163.com>
2019-07-05 15:48:20 +0800

    role button

--- src/store/modules/System/Role/RoleButton.js
+++ src/store/modules/System/Role/RoleButton.js
@@ -14,25 +14,18 @@ const RoleButton = {
 
   mutations: {
     SET_ROLE_BUTTON_LIST: (state, data) => {
-
       state.roleButtonList = data;
       state.roleButton.ACTION = data.filter(item => item.areaType === 'ACTION');
       state.roleButton.FROM = data.filter(item => item.areaType === 'FROM');
       state.roleButton.GRID = data.filter(item => item.areaType === 'GRID');
-      console.log(33333333333333);
-      console.log(state.roleButton);
-      console.log(33333333333333);
-
     },
   },
 
   actions: {
     getRoleButtonList: ({ commit }, requestHead) => {
-      console.log(1111111111);
       let btnlist = null;
       roleButtonList(requestHead)
         .then(response => {
-          console.log(222222222222);
           try {
             btnlist = response.resultData.map(item => {
               return {
@@ -42,7 +35,9 @@ const RoleButton = {
                 areaType: item.resourcebuttonType, //按钮区域类型
                 index: item.syOrderindex, //排序号
                 btnType: item.resourcebuttonBigiconcls, //按钮类型 （single / dropDown）
+                disabled: item.resourcebuttonDisabled === '1',//是否禁用
                 hide: item.resourcebuttonHidden === '1', //是否隐藏
+                icon: item.resourcebuttonIconcls,//按钮图标
               };
             });
           }
