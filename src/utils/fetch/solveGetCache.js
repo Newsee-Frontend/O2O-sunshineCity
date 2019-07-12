4f4ef5cab38b1a3099ce9f5b5564bf0312174ba4
xilanhuadechuntian <Lensgcx@163.com>
2019-07-12 18:28:44 +0800

    Update solveGetCache.js

--- src/utils/fetch/solveGetCache.js
+++ src/utils/fetch/solveGetCache.js
@@ -2,10 +2,10 @@ import { getBrowserInfo } from '../browser';
 import { uuid } from '../uuid';
 
 export default config => {
-  if (config.method === 'get' && getBrowserInfo().browser === 'IE') {
-    config.params = Object.assign(config.params, {
-      _: new Date().getTime,
+  if (config.method === 'get' && getBrowserInfo.browser === 'IE') {
+    config.params = Object.assign(config.params || {}, {
+      _: new Date().getTime(),
     });
   }
-  console.log(config.params);
+
 }
