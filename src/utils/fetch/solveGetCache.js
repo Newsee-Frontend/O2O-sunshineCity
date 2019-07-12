5ac3d8524e86dc574f1d11f1641af88597ba04f0
xilanhuadechuntian <Lensgcx@163.com>
2019-07-12 18:01:49 +0800

    2

--- src/utils/fetch/solveGetCache.js
+++ src/utils/fetch/solveGetCache.js
@@ -4,7 +4,7 @@ import { uuid } from '../uuid';
 export default config => {
   if (config.method === 'get' && getBrowserInfo().browser === 'IE') {
     config.params = {
-      _: uuid(10),
+      _: Date.now().getTime(),
     };
   }
 }
