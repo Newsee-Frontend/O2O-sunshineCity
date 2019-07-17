d7ff13efa66efe9e10530835153fa1caecc4f640
zhouxia <zhouxia@servyou.com.cn>
2019-07-17 15:16:22 +0800

    改名

--- src/router/modules/community.js
+++ src/router/modules/community.js
@@ -8,19 +8,19 @@ export default [
     children: [
       {
         path: 'activityList',
-        component: () => import('../../views/community/community-active/community-active.vue'),
+        component: () => import('../../views/Community/community-active/community-active.vue'),
         meta: {auth: true, key: 'activityList', title: '活动'},
         name: '活动管理',
       },
       {
         path: 'noticeList',
-        component: () => import('../../views/community/community-notice/community-notice.vue'),
+        component: () => import('../../views/Community/community-notice/community-notice.vue'),
         meta: {auth: true, key: 'noticeList', title: '公告'},
         name: '公告管理',
       },
       {
         path: 'precinctList',
-        component: () => import('../../views/community/community-precinct/community-precinct.vue'),
+        component: () => import('../../views/Community/community-precinct/community-precinct.vue'),
         meta: {auth: true, key: 'precinctList', title: '小区'},
         name: '小区设置',
       }
